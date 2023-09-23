696c0dc2353b0ec10b51e03a1f2bf521cb82bcee
Spike Soap <alazgorjs@gamil.com>
2023-09-24 01:05:15 +0200

    Creación de la modal para jugar

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+// Aquí irán todas las funcionalidades que tienen que ver con la modal
+
+
+// Constantes
+const modal = document.querySelector(".modal-container");
+const btnClose = document.querySelector(".btn-close");
+const btnEmpezar = document.querySelector(".btn-empezar");
+
+function mostrarModal() {
+    modal.style.display = "block";
+}
+
+setTimeout(mostrarModal, 2000);
+
+btnClose.addEventListener('click', ()=>{
+    modal.style.display = "none";
+});
+
+btnEmpezar.addEventListener('click', ()=>{
+
+    window.location.href = "pages/banderas.html";
+});
