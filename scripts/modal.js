5a6dd2e8d80d341a67bbdc4ddc6d1421fac96c82
Spike Soap <alazgorjs@gamil.com>
2023-10-08 18:48:44 +0200

    funcionalidad de la modal corregida

--- scripts/modal.js
+++ scripts/modal.js
@@ -5,12 +5,20 @@
 const modal = document.querySelector(".modal-container");
 const btnClose = document.querySelector(".btn-close");
 const btnEmpezar = document.querySelector(".btn-empezar");
+const logo = document.querySelector(".header-index_figure_img");
 
 function mostrarModal() {
     modal.style.display = "block";
 }
 
-setTimeout(mostrarModal, 2000);
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(mostrarModal, 1000);
+
+});
+
+logo.addEventListener("click", ()=>{
+    setTimeout(mostrarModal, 1000);
+})
 
 btnClose.addEventListener('click', ()=>{
     modal.style.display = "none";
